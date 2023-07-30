91082718195d04dda6ebd50a7fffafcd9bd21bdd
hasan13ek <108000428+hasan13ek@users.noreply.github.com>
2023-07-30 20:02:52 +0500

    some changes by Hasan

--- lib/features/main/prsentation/pages/cart/pages/cart_page.dart
+++ lib/features/main/prsentation/pages/cart/pages/cart_page.dart
@@ -38,18 +38,53 @@ class _CartPageState extends State<CartPage> {
                 itemCount: 5,
                 itemBuilder: (_, int index) {
                   return Padding(
-                    padding: AppUtils.kPaddingAll6,
+                    padding: AppUtils.kPaddingAll16,
                     child: Container(
                       width: double.infinity,
-                      height: 140,
+                      height: 120,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
+                        borderRadius: BorderRadius.circular(14),
                         color: ThemeColors.light.white,
                         boxShadow: [
                           BoxShadow(
                               color: ThemeColors.light.black.withOpacity(0.2),
-                              blurRadius: 2,
-                              offset: const Offset(0, 3))
+                              blurRadius: 6,
+                              offset: const Offset(0, 1))
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: double.infinity,
+                            width: 100,
+                            decoration: BoxDecoration(
+                                image: const DecorationImage(
+                                    image: NetworkImage(
+                                      "https://i.pinimg.com/236x/dd/b2/b6/ddb2b636719b747c52d62c7ee8574fd1.jpg",
+                                    ),
+                                    fit: BoxFit.cover),
+                                borderRadius: BorderRadius.circular(8)),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(
+                              left: 12,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(height: 12),
+                                Text("2020 Apple iPad Air 10.9"),
+                                SizedBox(height: 12),
+                                Text("\$579.00"),
+                                SizedBox(height: 12),
+                                Row(
+                                  children: [
+                                    Text("Quantity"),
+                                  ],
+                                )
+                              ],
+                            ),
+                          )
                         ],
                       ),
                     ),
